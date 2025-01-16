b18543b81487dc35b38511dfee1aa9101276d4c3
qaisar <qaisarfaryad45@gmail.com>
2025-01-16 22:38:43 +0500

    components imports are resolved

--- src/Components/components/ui/input.jsx
+++ src/Components/components/ui/input.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/components/lib/utils"
+import { cn } from "@/Components/lib/utils"
 
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
